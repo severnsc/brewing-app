ab5affbf81b1af8db54ec24cbb332b738e97195d
severnsc <severnsc@gmail.com>
2017-08-07 11:33:49 -0400

    Add styling for larger screens

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="flex-container">
+      <div className="body">
         <header className="header">
           <h2>App Name</h2>
         </header>
-        <main className="main">
-          <h2>App</h2>
-        </main>
-        <nav className="nav">
-          <ul>
-            <li><p>Inventory</p></li>
-            <li><p>Timer</p></li>
-          </ul>
-        </nav>
+        <div className="flex-container">
+          <main className="main">
+            <h2>App</h2>
+          </main>
+          <nav className="nav">
+            <ul>
+              <li><p>Inventory</p></li>
+              <li><p>Timer</p></li>
+            </ul>
+          </nav>
+        </div>
       </div>
     );
   }
