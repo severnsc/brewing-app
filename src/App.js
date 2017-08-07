af8de204ae351a56af7debfde7738a8a80cc9993
severnsc <severnsc@gmail.com>
2017-08-07 11:52:10 -0400

    Add user profile link to nav

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
             <ul>
               <li><p>Inventory</p></li>
               <li><p>Timer</p></li>
+              <li><p>Current User</p></li>
             </ul>
           </nav>
         </div>
