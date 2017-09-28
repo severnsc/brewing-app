6d173f99c5f335af1f90bd6b78c57a516ea404bf
severnsc <severnsc@gmail.com>
2017-09-28 18:03:23 -0400

    Make the timer form a separate component. Use currying for the handlesubmit function

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -6,6 +6,39 @@ const toTime = (minutes, seconds) => {
   return `${minutes}:${seconds}`
 }
 
+class TimerForm extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      minutes: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e){
+    this.setState({minutes: e.target.value})
+  }  
+
+  render(){
+    return(
+      <div className="componentContainer">
+        <h1>Create a New Timer</h1>
+        <form onSubmit={this.props.handleSubmit(this.state.minutes)} id="timerForm">
+          <div id="inputContainer">
+            <input type="text" value={this.state.minutes} id="minutesInput" onChange={this.handleChange} /><span id="minutes">minutes</span>
+          </div>
+          <div id="submitContainer">
+            <button type="submit" value="Create Timer" id="timerSubmit">
+              Create Timer
+            </button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
 class Timer extends Component{
 
   constructor(props){
@@ -129,13 +162,15 @@ class Timer extends Component{
     this.setState({editing: editing})
   }
 
-  handleSubmit(e){
-    e.preventDefault()
-    const ms = this.refs.minutes.value * 60000
-    this.setState({
-      time: ms,
-      initialTime: ms,
-    }, this.initializeTimer)
+  handleSubmit(minutes){
+    return (e) => {
+      e.preventDefault()
+      const ms = minutes * 60000
+      this.setState({
+        time: ms,
+        initialTime: ms,
+      }, this.initializeTimer)
+    }
   }
 
   initializeTimer(){
@@ -285,19 +320,7 @@ class Timer extends Component{
       )
     }else{
       return(
-        <div className="componentContainer">
-          <h1>Create a New Timer</h1>
-          <form onSubmit={this.handleSubmit} id="timerForm">
-            <div id="inputContainer">
-              <input type="text" ref="minutes" id="minutesInput" /><span id="minutes">minutes</span>
-            </div>
-            <div id="submitContainer">
-              <button type="submit" value="Create Timer" id="timerSubmit">
-                Create Timer
-              </button>
-            </div>
-          </form>
-        </div>
+        <TimerForm handleSubmit={this.handleSubmit} />
       )
     } 
   }
