a2d3dc4931b09065d9f9e17e756d1837d3154f4b
severnsc <severnsc@gmail.com>
2017-09-28 18:08:00 -0400

    Move TimerForm to a separate component file and require it in Timer.js

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,42 +1,10 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Alert from './Alert.js';
+import TimerForm from './TimerForm.js';
 
 const toTime = (minutes, seconds) => {
   return `${minutes}:${seconds}`
-}
-
-class TimerForm extends Component{
-
-  constructor(props){
-    super(props);
-    this.state = {
-      minutes: ''
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(e){
-    this.setState({minutes: e.target.value})
-  }  
-
-  render(){
-    return(
-      <div className="componentContainer">
-        <h1>Create a New Timer</h1>
-        <form onSubmit={this.props.handleSubmit(this.state.minutes)} id="timerForm">
-          <div id="inputContainer">
-            <input type="text" value={this.state.minutes} id="minutesInput" onChange={this.handleChange} /><span id="minutes">minutes</span>
-          </div>
-          <div id="submitContainer">
-            <button type="submit" value="Create Timer" id="timerSubmit">
-              Create Timer
-            </button>
-          </div>
-        </form>
-      </div>
-    )
-  }
 }
 
 class Timer extends Component{
