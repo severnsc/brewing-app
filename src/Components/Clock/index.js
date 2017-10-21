20d47929b34eed72e016ad5c16a4245e29528108
severnsc <severnsc@gmail.com>
2017-10-20 23:26:12 -0400

    Make Button and Clock functional components

--- src/Components/Clock/index.js
+++ src/Components/Clock/index.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 
-class Clock extends Component{
-
-  render(){
+const Clock = (props) => {
 
     const style = {fontSize: "8em"}
 
-    let seconds = this.props.seconds
+    let seconds = props.seconds
 
-    if(this.props.seconds < 10){
-      seconds = "0" + this.props.seconds
+    if(props.seconds < 10){
+      seconds = "0" + props.seconds
     }
 
     return(
-      <h2 style={style}>{this.props.minutes} : {seconds}</h2>
+      <h2 style={style}>{props.minutes} : {seconds}</h2>
     )
-  }
 
 }
 
