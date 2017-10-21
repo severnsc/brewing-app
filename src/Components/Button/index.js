d97bd811e7ed96c97993ca1c2f22ae1fc725938c
severnsc <severnsc@gmail.com>
2017-10-21 12:17:11 -0400

    Use function declaration not fat arrow

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.css'
 
-const Button = (props) => {
+function Button(props) {
     
     const buttonStyle = {
       backgroundColor: props.backgroundColor
