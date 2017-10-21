9c6e4f37571f01939eb79f0864efc3e5c1cd5e26
severnsc <severnsc@gmail.com>
2017-10-20 21:55:19 -0400

    Remove extra nav items

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -11,14 +11,7 @@ class Nav extends Component{
     return(
       <nav className={navClass}>
         <ul className="navItems">
-          <li><Link onClick={this.props.onClick} to='/inventory'><p>Inventory</p></Link></li>
-          <ul className="subItems">
-            <li><Link onClick={this.props.onClick} to='/inventory/malt'><p>Malt</p></Link></li>
-            <li><Link onClick={this.props.onClick} to='/inventory/hops'><p>Hops</p></Link></li>
-            <li><Link onClick={this.props.onClick} to='/inventory/yeast'><p>Yeast</p></Link></li>
-          </ul>
           <li><Link onClick={this.props.onClick} to='/timer'><p>Timer</p></Link></li>
-          <li><Link onClick={this.props.onClick} to='/profile'><p>Current User</p></Link></li>
         </ul>
       </nav>
     )
