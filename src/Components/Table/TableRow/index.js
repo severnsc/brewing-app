583c26326e9f27cdc53a146ef2ddbb1db3cdfa37
severnsc <severnsc@gmail.com>
2017-10-28 14:28:37 -0400

    Change propType requirements

--- src/Components/Table/TableRow/index.js
+++ src/Components/Table/TableRow/index.js
@@ -5,27 +5,31 @@ import Button from '../../Button/index.js'
 export default class TableRow extends Component{
 
   static propTypes = {
-    cellTypes: PropTypes.array.isRequired,
-    cellValues: PropTypes.array.isRequired
+    readOnly: PropTypes.bool,
+    editing: PropTypes.bool,
+    cells: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+      ]).isRequired,
+      type: PropTypes.string
+    })).isRequired
   }
 
   static defaultProps = {
-    editing: false
+    editing: false,
   }
 
   state = {
     editing: this.props.editing,
-    cellValues: this.props.cellValues
-  }
-
-  setDefaultCellValues = () => {
-    return this.props.cellTypes.map(() => {return ""})
+    cells: this.props.cells
   }
 
   handleChange = (e, index) => {
-    let cellValues = this.state.cellValues
-    cellValues[index] = e.target.value
-    this.setState({cellValues})
+    const cells = this.state.cells.map((cell, i) => {
+      return i === index ? {...cell, value: e.target.value} : cell
+    })
+    this.setState({cells})
   }
 
   handleSubmit = (e) => {
@@ -41,13 +45,13 @@ export default class TableRow extends Component{
     
     if(this.state.editing){
       
-      const cells = this.props.cellTypes.map((type, index) => {
+      const cells = this.state.cells.map((cell, index) => {
         return(
           <div className="tableCell" key={index}>
             <input
               key={index} 
-              type={type} 
-              value={this.state.cellValues[index]} 
+              type={cell.type} 
+              value={cell.value} 
               onChange={(e) => {this.handleChange(e, index)}}
             />
           </div>
@@ -59,6 +63,7 @@ export default class TableRow extends Component{
           <form onSubmit={this.handleSubmit}>
             {cells}
             <Button
+              onClick={() => {}}
               className="round"
               type="submit"
               buttonText="save"
@@ -70,10 +75,10 @@ export default class TableRow extends Component{
 
     }else{
       
-      const cells = this.state.cellValues.map((value, index) => {
+      const cells = this.state.cells.map((cell, index) => {
         return(
           <div className="tableCell" key={index}>
-            <p>{value}</p>
+            <p>{cell.value}</p>
           </div>
         )
       })
