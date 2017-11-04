c4aded47b1bd88caf72e057751d7fc5443b8b36a
severnsc <severnsc@gmail.com>
2017-11-04 12:07:43 -0400

    Add prop types to InventoryTable

---
+++ src/Components/InventoryTable/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Table from '../Table'
+import PropTypes from 'prop-types'
+
+const InventoryTable = ({tableRows, addRow}) => {
+
+  const columns = [
+    {name: "Name", type: "text"},
+    {name: "Amount", type: "number"}
+  ]
+
+  const tableRowComponents = tableRows.map((tableRow) => {
+    return <TableRow cells={tableRow.cells} editing={tableRow.editing} />
+  })
+
+  return(
+    <Table 
+      columns={columns} 
+      name="maltInventory" 
+      addRow={addRow}
+    >
+      {tableRowComponents}
+    </Table>
+  )
+}
+
+InventoryTable.propTypes = {
+  tableRows: PropTypes.arrayOf(shape({
+    id: PropTypes.number.isRequired,
+    tableName: PropTypes.string.isRequired,
+    editing: PropTypes.bool.isRequired,
+    readOnly: PropTypes.bool,
+    cells: PropTypes.arrayOf(shape({
+      tableRowId: PropTypes.number.isRequired,
+      value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+      ]).isRequired,
+      type: PropTypes.string
+    })).isRequired
+  })),
+  addRow: PropTypes.func
+}
+
+export default InventoryTable
