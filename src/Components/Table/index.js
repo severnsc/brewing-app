0aa62b84393e8a9570f10b44344fa7d8db969c39
severnsc <severnsc@gmail.com>
2017-11-04 12:14:33 -0400

    Make table functional component

--- src/Components/Table/index.js
+++ src/Components/Table/index.js
@@ -3,46 +3,43 @@ import PropTypes from 'prop-types'
 import TableRow from './TableRow/index.js'
 import Button from '../../Components/Button/index.js'
 
-export default class Table extends Component {
-
-  static propTypes = {
-    readOnly: PropTypes.bool,
-    name: PropTypes.string.isRequired,
-    addRow: PropTypes.func.isRequired,
-    columns: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      type: PropTypes.string
-    }).isRequired).isRequired 
-  }
-
-  state = {
-    rows: Array.isArray(this.props.children) ? this.props.children : [this.props.children]
-  }
+const Table = ({readOnly, name, addRow, columns, children}) => {
 
   addRow = () => {
-    const cells = this.columns.map((column) => {
+    const cells = columns.map((column) => {
       return {value: "", type: column.type}
     })
     const newRow = {
-      tableName: this.props.name,
+      tableName: name,
       editing: true,
       readOnly: false,
       cells
     }
-    this.props.addRow(newRow)
+    addRow(newRow)
   }
 
   render(){
-    const { columns, readOnly } = this.props
     return(
       <div>
         <TableRow 
           readOnly 
           cells={columns.map((col) => {return {value: col.name, type: col.type || "text"}})}
         />
-        {this.state.rows}
-        {readOnly || <Button className="round" buttonText="+" onClick={this.addRow} />}
+        {children}
+        {readOnly || <Button className="round" buttonText="+" onClick={addRow} />}
       </div>
     )
   }
 }
+
+Table.propTypes = {
+  readOnly: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  addRow: PropTypes.func.isRequired,
+  columns: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string
+  }).isRequired).isRequired 
+}
+
+export default Table
