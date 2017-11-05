442ae7d0f4869699f2b81d7baa9e52aafb95fd5b
severnsc <severnsc@gmail.com>
2017-11-04 22:30:15 -0400

    Add provider and store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import brewingApp from './reducers'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let store = createStore(brewingApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
