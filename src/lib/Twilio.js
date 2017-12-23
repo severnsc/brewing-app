1ba918e5ae54e7d3492eedcf4fbbdc82bbba0e40
severnsc <severnsc@gmail.com>
2017-12-23 11:31:45 -0500

    Remove unnecessary parens

--- src/lib/Twilio.js
+++ src/lib/Twilio.js
@@ -2,19 +2,15 @@ require('dotenv').config()
 const twilio = require('twilio')
 const client = new twilio(process.env.SID, process.env.TOKEN)
 
-const sendSMS = (message) => {
+const sendSMS = message => {
   
   client.api.messages.create({
     body: message,
     to: process.env.TO_PHONE,
     from: process.env.FROM_PHONE
   })
-  .then((mes) => {
-    console.log(mes.sid)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+  .then(mes => console.log(mes.sid))
+  .catch(err => console.log(err))
 
 }
 
