36d8fedbbc66bbf1bc26ba81933256104ef92b62
severnsc <severnsc@gmail.com>
2017-12-24 15:13:10 -0500

    Add redux middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import brewingApp from './reducers'
 import Body from './App'
 import './index.css'
 
-let store = createStore(brewingApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+let store = createStore(
+  brewingApp,
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 render(
   <Provider store={store}>
